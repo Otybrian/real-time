add6f880e698eb799b76041ddd0c13004ef18cf5
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-19 01:35:31 +0300

    finish updating views and urls

--- hood/urls.py
+++ hood/urls.py
@@ -5,6 +5,16 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.index,name = 'index'), 
+    path('profile/', views.profile, name='profile'),
+    path('new_neighbor',views.new_neighbor, name= 'new_neighbor'),
+    path('neighborhood', views.neighborhood, name = 'neighborhood'),
+    path('hood/<str:name>',views.single_hood,name='single_hood'),
+    path('join_hood/<int:id>', views.join_hood, name='join'),
+    path('leave_hood/<int:id>', views.leave_hood, name='leave'),
+    path("business/create/", views.create_business, name="create_business"), # create business
+    path('business/', views.business, name = 'business'),
+    path('search/', views.search, name='search_results'),
+    path('post/new-post', views.create_post, name='create_post'),
 ]
 
 
--- hood/views.py
+++ hood/views.py
@@ -22,8 +22,97 @@ def profile(request):
         form = ProfileForm(instance=request.user.profile)
     return render(request, 'main/profile.html', {"form":form,'profile':profile})
 
-    
+
 def index(request):
     posts = Post.objects.all().order_by("-id")
     return render(request, 'main/index.html', {'posts': posts})
+
+@login_required(login_url="/accounts/login/")
+def new_neighbor(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NeighborhoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            neighbor = form.save(commit=False)
+            neighbor.user = current_user
+            neighbor.save()
+        return HttpResponseRedirect('/neighborhood')
+    else:
+        form = NeighborhoodForm()
+    return render(request, 'main/create_neighbor.html',{'form':form})
+@login_required(login_url="/accounts/login/")
+def neighborhood(request):
+    current_user = request.user
+    neighborhood = Neighborhood.objects.all().order_by('-id')
+    return render(request, 'main/neighborhoods.html', {'neighborhood': neighborhood})
+
+@login_required(login_url='/accounts/login/')
+def single_hood(request,name):
+    hood = Neighborhood.objects.get(name=name)
+    business = Business.objects.filter(neighborhood=hood)
+    post = Post.objects.filter(neighborhood=hood)
+
+
+    return render(request,'main/single_hood.html',{'hood':hood,'businesses':business,'posts':post})
+
+def join_hood(request,id):
+    neighborhood = get_object_or_404(Neighborhood, id=id)
+    
+    request.user.profile.neighborhood = neighborhood
+    request.user.profile.save()
+    return redirect('neighborhood')
+
+def leave_hood(request, id):
+    hood = get_object_or_404(Neighborhood, id=id)
+    request.user.profile.neighborhood = None
+    request.user.profile.save()
+    return redirect('neighborhood')
+
+@login_required(login_url="/accounts/login/")
+def create_business(request):
+    current_user = request.user
+    if request.method == "POST":
+        form = BusinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.user = current_user
+            business.save()
+        return HttpResponseRedirect('/neighborhood')
+    else:
+        form = BusinessForm()
+    return render(request, "main/add_business.html", {'form':form})
+
+@login_required(login_url="/accounts/login/")
+def business(request):
+    current_user = request.user
+    business = Business.objects.all().order_by('-id')
+    return render(request, 'main/business.html', {'businesses': business})
+
+@login_required(login_url='/accounts/login/')
+def search(request):
+    if 'search' in request.GET and request.GET['search']:
+        search_term = request.GET.get('search').lower()
+        business = Business.search_by_name(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'main/search.html', {'message': message, 'business':business})
+    else:
+        message = 'Not found'
+    return render(request, 'main/search.html', {'message': message})
+
+
+@login_required(login_url="/accounts/login/")
+def create_post(request):
+    current_user = request.user
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+
+        return HttpResponseRedirect('/neighborhood')
+    else:
+        form = PostForm()
+    return render(request, "main/create_post.html", {'form':form})
 
