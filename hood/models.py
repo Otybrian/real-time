b891fb8ccac79507b10c267086634470d4cc768a
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-18 22:33:53 +0300

    create profile model

--- hood/models.py
+++ hood/models.py
@@ -4,3 +4,32 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    profile_pic = CloudinaryField('image')
+    bio = models.TextField(max_length=300,null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,null=True)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE,related_name='neighbors',null=True)
+
+    def str(self):
+        return f'{self.user.username} profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    
+
+    def update_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
