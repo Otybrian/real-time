5737a1fb696f2d53e623b6b15fb6192d20b6eeed
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-18 22:37:35 +0300

    add location model

--- hood/models.py
+++ hood/models.py
@@ -33,3 +33,15 @@ class Profile(models.Model):
 
     def delete_profile(self):
         self.delete()
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+        
