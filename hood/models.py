76ca1d47c915b6badad159f5ed0e5f9af2977d2b
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-18 22:41:22 +0300

    add model class Neighborhood

--- hood/models.py
+++ hood/models.py
@@ -4,7 +4,53 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.name 
+        
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=50)
+    image = CloudinaryField('image',null=True)
+    description= models.TextField(max_length=300,null=True)
+    occupants = models.IntegerField(default=0)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,null=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    health_tell = models.IntegerField(null=True, blank=True)
+    police_tell = models.IntegerField(null=True, blank=True)
+    area_administrator = models.CharField(max_length=100,null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def create_neigborhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighborhood(cls, id):
+        cls.objects.filter(id=id).delete()
+
+    @classmethod
+    def update_neighborhood(cls, id):
+        cls.objects.filter(id=id).update()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        hood = cls.objects.filter(name__icontains=search_term)
+        return hood
+
+    @classmethod
+    def find_neigborhood(cls, id):
+        hood = cls.objects.get(id=id)
+        return hood
+
+    def __str__(self):
+        return self.name
 
 class Profile(models.Model):
     name = models.CharField(max_length=50)
@@ -34,14 +80,10 @@ class Profile(models.Model):
     def delete_profile(self):
         self.delete()
 
-class Location(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def save_location(self):
-        self.save()
-
     def __str__(self):
-        return self.name
-        
+        return self.name      
