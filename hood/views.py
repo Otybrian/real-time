7c8b06e1267419b7697b269f0d71cf25b94c047f
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-19 01:30:24 +0300

    start updating views

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from .models import Neighborhood, Profile,Business,Location,Category,Post
 
 # Create your views here.
 def index(request):
-    welcome='Welcome to my Hood'
-    return render(request, 'index.html', {'welcome':welcome})
+    posts = Post.objects.all().order_by("-id")
+    return render(request, 'main/index.html', {'posts': posts})
+
