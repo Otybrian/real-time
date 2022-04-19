d6c477ca36afe050841f03fa69c60042dfdb19f4
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-19 11:15:54 +0300

    finish adding the register and login pages

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -145,7 +145,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = 'index'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
